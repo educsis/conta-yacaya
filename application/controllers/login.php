ff11c6c2da9723614a65fecbb101bd37dd497ae1
educsis <educsis@gmail.com>
2014-11-05 21:17:19 -0600

    Login views y controladores

---
+++ application/controllers/login.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+
+	public function index()
+	{
+        $data = array(
+            'datos' =>  ''
+        );
+		$this->load->view('login_view', $data);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/login_view.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+
+  <title>conta.YacaYa.com .::. Login</title>
+
+  <link href="assets/css/style.default.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body class="signin">
+
+<!-- Preloader -->
+<div id="preloader">
+    <div id="status"><i class="fa fa-spinner fa-spin"></i></div>
+</div>
+
+<section>
+
+    <div class="signinpanel">
+
+        <div class="row">
+
+            <div class="col-md-7">
+
+                <div class="signin-info">
+                    <div class="logopanel">
+                        <h1><span>[</span> conta.YacaYa <span>]</span></h1>
+                    </div><!-- logopanel -->
+
+                    <div class="mb20"></div>
+
+                    <h5><strong>Bienvenidos a conta.YacaYa</strong></h5>
+                    <ul>
+                        <li><i class="fa fa-arrow-circle-o-right mr5"></i> Contabilidad inteligente</li>
+                        <li><i class="fa fa-arrow-circle-o-right mr5"></i> Reportes</li>
+                        <li><i class="fa fa-arrow-circle-o-right mr5"></i> Data seguro</li>
+                        <li><i class="fa fa-arrow-circle-o-right mr5"></i> Interfaz pr&aacute;ctica</li>
+                        <li><i class="fa fa-arrow-circle-o-right mr5"></i> y mucho m&aacute;s...</li>
+                    </ul>
+                    <div class="mb20"></div>
+                    <strong>Quieres una cuenta? <a href="signup.html">Subscribete</a></strong>
+                </div><!-- signin0-info -->
+
+            </div><!-- col-sm-7 -->
+
+            <div class="col-md-5">
+
+                <form method="post" action="http://themepixels.com/demo/webpage/bracket/index.html">
+                    <h4 class="nomargin">Login</h4>
+                    <p class="mt5 mb20">Entra a tu cuenta</p>
+
+                    <input type="text" class="form-control uname" placeholder="Usuario" />
+                    <input type="password" class="form-control pword" placeholder="Clave" />
+                    <a href="#"><small>No recuerdas tu clave?</small></a>
+                    <button class="btn btn-success btn-block">Entrar</button>
+
+                </form>
+            </div><!-- col-sm-5 -->
+
+        </div><!-- row -->
+
+        <div class="signup-footer">
+            <div class="pull-left">
+                &copy; 2014. Todos los derechos reservados. conta.YacaYa v.0.01
+            </div>
+            <div class="pull-right">
+                Creado por: <a href="http://yacaya.com/" target="_blank">Yacaya.com</a>
+            </div>
+        </div>
+
+    </div><!-- signin -->
+
+</section>
+
+
+<script src="assets/js/jquery-1.10.2.min.js"></script>
+<script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/modernizr.min.js"></script>
+<script src="assets/js/retina.min.js"></script>
+
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
