a8f802b96d8b8e73f83f20f53f121a5ac7f6cabc
solucionespm <edwin@solucionespm.com>
2014-11-06 17:16:47 -0600

    autoload sparks

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -110,7 +110,7 @@ $autoload['language'] = array();
 */
 
 $autoload['model'] = array();
-
+$autoload['sparks'] = array('ion_auth/2.5.0');
 
 /* End of file autoload.php */
 /* Location: ./application/config/autoload.php */
